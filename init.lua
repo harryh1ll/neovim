cf899420f8ce1ff33ad1373fd9891181a29165d4
harryh1ll <g.hill18@imperial.ac.uk>
2025-03-24 22:06:33 +0000

    changes

--- init.lua
+++ init.lua
@@ -1,7 +1,22 @@
 require("config.lazy")
 vim.wo.number = true
 vim.opt.colorcolumn = "100"
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
 vim.g.python3_host_prog = '/Users/harryhill/.pyenv/shims/python'
+
+
+local function set_tab_width(filetype, tabstop, shiftwidth, expandtab)
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = filetype,
+    callback = function()
+      vim.opt_local.tabstop = tabstop
+      vim.opt_local.shiftwidth = shiftwidth
+      vim.opt_local.expandtab = expandtab -- true for spaces, false for tabs
+    end,
+  })
+end
+
+
+set_tab_width("python", 4, 4, true)
+set_tab_width("c", 2, 2, true)
+set_tab_width("rust", 4, 4, true)
+
